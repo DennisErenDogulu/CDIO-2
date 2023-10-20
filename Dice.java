7f74527d80b7b2e2246a00c69ac2f3319b36c2ab
DennisErenDogulu <144697291+DennisErenDogulu@users.noreply.github.com>
2023-10-20 16:02:59 +0200

    Added die code, and tutorial in file to change dice

--- Dice.java
+++ Dice.java
@@ -1,1 +1,18 @@
+import java.util.random;
 
+Public class Die{
+    public int faceValue;
+    public Random random = new random();
+
+    public Die()[
+
+    ]
+}
+
+public int roll(){
+    // to change the dice from a six sided dice to the dice you want. Change the value in the () next to nextInt.
+    // The value put in the brackets decide which die is used so ie. a six sided die requires 6 in the brackets a 12 sided
+    // die requires you to put 12 in the brackets.
+    faceValue = random.nextInt(6)+1;
+    return faceValue;
+}
