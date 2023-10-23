ccaa5f7a83cb51c4639a9198385e70a1f92c9684
Dennis Dogulu <eren@hotmail.dk>
2023-10-23 15:11:38 +0200

    Made Dice values

--- Dicevalues.java
+++ Dicevalues.java
@@ -1,1 +1,24 @@
+public class Dicevalues {
+    public int Dice_1;
+    public int Dice_2;
+    public int sum;
 
+    public int Dievalue() {
+        Dice_1 = new Die().roll();
+        Dice_2 = new Die().roll();
+
+        return Dice_1 + Dice_2;
+    }
+
+    public int sumFace() {
+        sum = Dice_1 + Dice_2;
+        return sum;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public String toStringValueOfDies() {
+        return "First roll is " + Dice_1 + " Second roll is " + Dice_2;
+    }}
