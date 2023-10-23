a714cb26673231cbe0fae8cbf48620aaa9faaa9e
DennisErenDogulu <144697291+DennisErenDogulu@users.noreply.github.com>
2023-10-23 14:26:20 +0200

    Started on interratike part of main

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public static void Main (String args[]){
 
+Scanner scanner = new Scanner(System.in);
+System.out.println("Hello, and welcome to the dice game. If you are ready to play the dicegame then please write Yes in the terminal");
+
+var answar = scanner.nextLine();
+if(answar.equals("Yes" || "yes" ))
+    System.out.println("Hello ")
+
+
+else{
+    System.out.println("I don't understand, you don't want to play the game?")
+}
+scanner.close();
+}
