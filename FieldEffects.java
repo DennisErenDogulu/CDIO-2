b9276e97ca7fe9c85690b77d488d6dcd4c2b9dd8
s235080 <nikoplund@gmail.com>
2023-10-24 15:16:16 +0200

    Changes in FieldEffect

--- FieldEffects.java
+++ FieldEffects.java
@@ -1,31 +1,45 @@
 public class FieldEffects {
-    
-    public static int getEffect(int rollResult) {
+
+    public int getEffect(int rollResult) {
         int amount;
         switch (rollResult) {
             case 2:
                 amount = 250;
+                break;
             case 3:
                 amount= -100;
+                break;
             case 4:
                 amount = 100;
+                break;
             case 5:
                 amount = -20;
+                break;
             case 6:
                 amount = 180;
+                break;
             case 7:
                 amount = 0;
+                break;
             case 8:
                 amount = -70;
+                break;
             case 9:
                 amount = 60;
+                break;
             case 10:
-                amount = -80; 
+                amount = -80;
+                break;
             case 11:
                 amount = -50;
+                break;
             case 12:
                 amount = 650;
+                break;
+            default:
+                amount = 0; // or any other default value or action
+                break;
         }
+        return amount;
     }
 }
-
